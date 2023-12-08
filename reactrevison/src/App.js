dcb355b9cda68c0cac4e7ede5b8998a5912cc664
keshavthakur0404@gmail.com <keshavthakur1>
2023-12-08 10:59:04 +0530

    Initial commit

---
+++ reactrevison/src/App.js
@@ -0,0 +1,15 @@
+// app.js
+import Chai from "./Chai.js";
+
+function App() {
+    return (
+      <> 
+     <Chai/>
+      <h1>mr Thakur in India </h1>
+        </>
+    );
+}
+//yaha hamne sikha function ka name captilaize rakho
+//EVEN file ka name bhi rakho captilize even effect nhi dega
+export default App;
+  
---
+++ reactrevison/src/Chai.js
@@ -0,0 +1,12 @@
+
+
+function Chai() {
+    return (
+        <div>
+        <h3>chai is ready</h3>
+        <h1>mr Thakur in India </h1>
+        </div>
+    )
+}
+
+export default Chai;
